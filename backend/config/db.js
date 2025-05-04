f4ab999db287d14c9a5f7c1be4cb3a7ca9ff11d8
HamzaEhtisham <hamzaehtisham124@gmail.com>
2025-05-04 14:27:20 +0500

    Fix mixed content issue by using HTTPS

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
+        mongoose.set('strictQuery', false); // Add this line to suppress the warning
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
